857a6688f360251851a17d5bcb5d133e23d0e714
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2020-11-28 12:42:58 +0100

    Tokyo colorscheme

--- config.h
+++ config.h
@@ -13,8 +13,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#1d2021" },
-	[SchemeSel] = {  "#eeeeee", "#fb4934" },
+	[SchemeNorm] = { "#a9b1d6", "#3b3d57" },
+	[SchemeSel] = {  "#eeeeee", "#ff7a93" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
