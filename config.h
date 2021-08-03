36fd1cd7f6a74429b360ae1a1943d7847150db5c
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2021-08-03 16:07:13 +0200

    change colors and font

--- config.h
+++ config.h
@@ -7,14 +7,14 @@ static int min_width = 500;                    /* minimum width when centered */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono:size=12",
+	"JetBrains Mono:size=18",
     "JoyPixels:pixelsize=14:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#a9b1d6", "#3b3d57" },
-	[SchemeSel] = {  "#eeeeee", "#ff7a93" },
+	[SchemeNorm] = { "#ffffff", "#000000" },
+	[SchemeSel] = {  "#eeeeee", "#bababa" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
